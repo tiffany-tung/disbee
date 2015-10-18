6d8b115d38c388a792042a03c9477b736ca3013a
Marcus Scott <mmmscott@uw.edu>
2015-10-17 21:49:58 -0700

    add component stubs

--- client/js/index.es6.js
+++ client/js/index.es6.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-
+import Post from './post.es6.js';
 import Root from './containers/Root'
 import setUpRealtime from './Realtime.es6.js'
 
-ReactDOM.render(<Root />, document.getElementById('container'));
+ReactDOM.render(<Post />, document.getElementById('container'));
 
 setUpRealtime();
---
+++ client/js/tags.es6.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Tags extends React.Component {
+	constructor() {
+		super()
+	}
+
+	render() {
+		return <div>HAHA</div>
+	}
+}
